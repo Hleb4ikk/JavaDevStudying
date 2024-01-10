3dec7d00c5af1f9232917b64c985b06c8579c9a4
Hleb4ikk <hlebor2006@mail.ru>
2024-01-10 21:09:37 +0300

    update the inheritance

--- src/main/java/inheritance/Main.java
+++ src/main/java/inheritance/Main.java
@@ -3,8 +3,8 @@ package inheritance;
 public class Main {
     public static void main(String[] args){
 
-        employee Dev = new employee("Gleb", 18, 1.92);
-        System.out.println(Dev.getName());
+        employee Dev = new employee("Gleb", 18, 1.92, "93jdskg", "hlebor2006@mail.ru");
+        System.out.println(Dev.getName() + " " + Dev.getEmail() + " " + Dev.getPassword());
 
     }
 }
--- src/main/java/inheritance/employee.java
+++ src/main/java/inheritance/employee.java
@@ -1,9 +1,27 @@
 package inheritance;
 
 public class employee extends Human { //extends <Название класса свойства, методы и конструктор которого надо наследовать>
-                                      //^^^^^^^
-                                      //Ключевое слово которое обозначает наследование.
-    public employee(String name, int age, double height) { //В параметрах нового конструктора обяательно должны быть указаны параметры от унаследованного
+    private String password;          //^^^^^^^
+    private String email;             //Ключевое слово которое обозначает наследование.
+    public employee(String name, int age, double height, String password, String email) { //В параметрах нового конструктора обяательно должны быть указаны параметры от унаследованного
         super(name, age, height);     //super - обозначает вызов конструктора с тремя аргументами класса Human, также должны быть указаны все переменные из объявленного выше конструктора объектов класса.
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
