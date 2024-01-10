b194b02feb25b454905e511fd40866314b8697ef
Hleb4ikk <hlebor2006@mail.ru>
2024-01-10 20:53:58 +0300

    ÐInheritence JAVA OOP

---
+++ src/main/java/inheritance/Main.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args){
+
+        employee Dev = new employee("Gleb", 18, 1.92);
+        System.out.println(Dev.getName());
+
+    }
+}
